8832985b23823732245cfc004a60bf15b4ca656a
Matas Grišius <matas.grisius@gmail.com>
2019-10-01 20:45:50 +0300

    init commit

---
+++ Controllers/PicturesController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using pix.Entities;
+
+namespace pix.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PicturesController : ControllerBase
+    {
+        private readonly PixDbContext _context;
+
+        public PicturesController(PixDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Pictures
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Picture>>> GetPictures()
+        {
+            return await _context.Pictures.ToListAsync();
+        }
+
+        // GET: api/Pictures/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Picture>> GetPicture(int id)
+        {
+            var picture = await _context.Pictures.FindAsync(id);
+
+            if (picture == null)
+            {
+                return NotFound();
+            }
+
+            return picture;
+        }
+
+        // PUT: api/Pictures/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPicture(int id, Picture picture)
+        {
+            if (id != picture.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(picture).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PictureExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Pictures
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for
+        // more details see https://aka.ms/RazorPagesCRUD.
+        [HttpPost]
+        public async Task<ActionResult<Picture>> PostPicture(Picture picture)
+        {
+            _context.Pictures.Add(picture);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetPicture", new { id = picture.Id }, picture);
+        }
+
+        // DELETE: api/Pictures/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Picture>> DeletePicture(int id)
+        {
+            var picture = await _context.Pictures.FindAsync(id);
+            if (picture == null)
+            {
+                return NotFound();
+            }
+
+            _context.Pictures.Remove(picture);
+            await _context.SaveChangesAsync();
+
+            return picture;
+        }
+
+        private bool PictureExists(int id)
+        {
+            return _context.Pictures.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ Entities/PixDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace pix.Entities
+{
+    public class PixDbContext : DbContext
+    {
+        public PixDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Picture> Pictures { get; set; }
+    }
+}
